29c205b77a46929078f44f9190b352e6d3cd9421
ElguetaM <vivianaandrea09@hotmail.com>
2024-01-26 19:39:49 -0300

    Agregado de smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,21 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
